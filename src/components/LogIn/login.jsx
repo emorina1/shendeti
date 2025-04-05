56c0028838530c47f624f294c15662d5a3a3e641
emorina1 <151447538+emorina1@users.noreply.github.com>
2025-04-05 15:24:55 +0200

    change login style
    stilizimi i login formes

--- src/components/LogIn/login.jsx
+++ src/components/LogIn/login.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
+import "./auth.css";
+
 
 const Login = () => {
   const [values, setValues] = useState({
