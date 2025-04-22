0c76c0fe68975e2a87316101ef745ffa21022669
emorina1 <151447538+emorina1@users.noreply.github.com>
2025-04-22 16:15:45 +0200

    Home update

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,19 +1,16 @@
 import React, { useState, useEffect } from "react";
 import "./Home.css";
-import homeImage1 from "../../imgs/Doki3.png";
+
 import homeImage2 from "../../imgs/Doki1.png";
 import homeImage3 from "../../imgs/Doki2.png";
 
-const images = [homeImage1, homeImage2, homeImage3];
+
+const images = [ homeImage2, homeImage3];
 
 const quotes = [
-  {
-    text: "“Shëndeti është pasuria më e madhe.”"
-   
-  },
+ 
   {
     text: "“Një mendje e shëndoshë qëndron në një trup të shëndoshë.”"
-   
   },
   {
     text: "“Kujdesu për trupin tënd, është i vetmi vend ku duhet të jetosh.”"
@@ -27,14 +24,14 @@ const Home = () => {
   useEffect(() => {
     const timer = setTimeout(() => {
       setShowWelcomePage(false);
-    }, 3000); // 3 sekonda
+    }, 3000);
     return () => clearTimeout(timer);
   }, []);
 
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentImage((prev) => (prev + 1) % images.length);
-    }, 4000); // çdo 4 sekonda
+    }, 4000);
     return () => clearInterval(interval);
   }, []);
 
@@ -42,14 +39,26 @@ const Home = () => {
     <div id="home" className="home-container">
       {showWelcomePage ? (
         <section className="welcome-page">
-          <h1>Mirësevini në Shëndetin Tuaj!</h1>
-          <p>Ne jemi këtu për t'ju udhëzuar në rrugën drejt një jete më të shëndetshme dhe më të lumtur.</p>
+        <video
+  src="/Video.mp4"
+  autoPlay
+  muted
+  loop
+  className="welcome-video"
+/>
+
+          <div className="welcome-text">
+            <h1>Mirësevini në Shëndetin Tuaj!</h1>
+            <p>
+              Ne jemi këtu për t'ju udhëzuar në rrugën drejt një jete më të
+              shëndetshme dhe më të lumtur.
+            </p>
+          </div>
         </section>
       ) : (
         <section className="hero">
           <div className="hero-content">
             <h1 className="hero-title quote-text">{quotes[currentImage].text}</h1>
-            <p className="hero-author">— {quotes[currentImage].author}</p>
           </div>
 
           <div className="hero-image-container">
