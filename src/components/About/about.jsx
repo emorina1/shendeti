d81ed594e27411a5478d3eb1926eb029630a8962
emorina1 <151447538+emorina1@users.noreply.github.com>
2025-05-01 14:41:26 +0200

    Update components

--- src/components/About/about.jsx
+++ src/components/About/about.jsx
@@ -1,31 +1,37 @@
 import React from 'react';
 import './about.css';
-import aboutImage from '../../imgs/photo.png'; // Shto një foto në projektin tënd
+import aboutImage from '../../imgs/photo.png'; // Përdorimi i imazhit në mënyrë të duhur
+// Nuk e përdorim më link brenda JSX, e vendosim në index.html për Google Fonts
 
 const AboutUs = () => {
   return (
-<div id="about" className="about-section">
+    <div id="about" className="about-section">
       {/* Seksioni i kutive (cards) */}
+      <h1 className="jo">Welcome to Healthy Life</h1>
+      <p className="po">Our goal is to provide you with everything you need for a healthier lifestyle. From fitness advice to nutritious meals, we offer guidance to help you make the best decisions for your well-being. Stay healthy, stay happy! Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore laboriosam deserunt, illo eum laudantium incidunt, ea ipsam fugit provident autem numquam praesentium nulla harum dolorem dignissimos tempore asperiores quia quibusdam?</p>
+
+
       <div className="info-cards">
-        <div className="card light-blue">
-          <img src="emergency-icon.png" alt="Emergency Icon" className="card-icon" />
-          <h3>Emergency Cases</h3>
-          <p>There are many variations of passages of Lorem Ipsum available...</p>
-          <p className="phone-number">📞 987 654 321</p>
-        </div>
+      <div className="card light-blue">
+  <i className="fas fa-ambulance card-icon"></i> {/* Ikona e emergjencës */}
+  <h3>Emergency Cases</h3>
+  <p>There are many variations of passages of Lorem Ipsum available...</p>
+  <p className="phone-number">📞 987 654 321</p>
+</div>
 
-        <div className="card dark-blue">
-          <img src="doctor-icon.png" alt="Doctor Icon" className="card-icon" />
-          <h3>Doctors Timetable</h3>
-          <p>The majority have suffered alteration in some form...</p>
-          <button className="card-button">Timetable ➝</button>
-        </div>
+<div className="card dark-blue">
+  <i className="fas fa-user-md card-icon"></i> {/* Ikona e doktorit */}
+  <h3>Doctors Timetable</h3>
+  <p>The majority have suffered alteration in some form...</p>
+  <button className="card-button">Timetable ➝</button>
+</div>
 
-        <div className="card light-blue">
-          <img src="clock-icon.png" alt="Clock Icon" className="card-icon" />
-          <h3>Opening Hours</h3>
-          <p>
-            Monday - Friday: 8:00 - 7:00 PM <br />
+<div className="card light-blue">
+  <i className="fas fa-clock card-icon"></i> {/* Ikona e orës */}
+  <h3>Opening Hours</h3>
+  <p>Monday - Friday: 8:00 - 7:00 PM
+
+
             Saturday: 6:00 - 5:00 PM <br />
             Sunday: 8:00 - 2:00 PM <br />
             Emergency: 24H/7Days
--- src/components/Healthy/healthy.jsx
+++ src/components/Healthy/healthy.jsx
@@ -4,33 +4,32 @@ import './healthy.css';
 const Healthy = () => {
   return (
     <div id="healthy" className="healthy-section">
-  
       <h2 className="healthy-title">Easily Book Your Doctor</h2>
       <p className="healthy-subtitle">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad molestiae ut.
+        Book your doctor appointment effortlessly with a few clicks!
       </p>
-      
+
       <div className="healthy-cards">
         <div className="healthy-card">
-          <img src="vaccination-icon.png" alt="Vaccination" className="healthy-icon" />
+          <i className="fas fa-syringe healthy-icon"></i> {/* Ikona e vaksinës */}
           <h3>Vaccination</h3>
           <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
         </div>
 
         <div className="healthy-card highlighted">
-          <img src="emergency-icon.png" alt="Emergency" className="healthy-icon" />
+          <i className="fas fa-ambulance healthy-icon"></i> {/* Ikona e emergjencës */}
           <h3>Emergency</h3>
           <p>Lorem ipsum is simply dummy text of the printing industry.</p>
         </div>
 
         <div className="healthy-card">
-          <img src="medical-icon.png" alt="Medical Center" className="healthy-icon" />
+          <i className="fas fa-hospital-alt healthy-icon"></i> {/* Ikona e qendrës mjekësore */}
           <h3>Medical Center</h3>
           <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
         </div>
 
         <div className="healthy-card">
-          <img src="doctor-icon.png" alt="Doctor Specialist" className="healthy-icon" />
+          <i className="fas fa-user-md healthy-icon"></i> {/* Ikona e doktorit */}
           <h3>Doctor Specialist</h3>
           <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
         </div>
